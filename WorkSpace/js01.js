0ccc92fdb8f093d4b6e4bb086afcc650b08b189f
v1t2m3 <nguyenvantamdn@gmail.com>
2021-04-12 20:33:09 +0700

    Bài tập thực hành

---
+++ WorkSpace/js01.js
@@ -0,0 +1,24 @@
+let f = 20.5
+let b = true
+let s = 'Hà Nội'
+
+document.write('f = ' + f)
+document.write('<br/>')
+document.write('b = ' + b)
+document.write('<br/>')
+document.write('s = ' + s)
+
+document.write('<br/>')
+let width = prompt("Nhập chiều rộng")
+let height = prompt("Nhập chiều dài")
+let area = width * height
+document.write('Diện tích = ' + area)
+
+let soa = prompt("Nhập số a")
+let sob = prompt("Nhập số b")
+let soc
+if  (soa%sob==0) {
+    alert('Số a là bội số của b')
+} else {
+    alert('Số a không là bội số của b')
+}
